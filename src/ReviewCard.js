fb9c53457fec063b103a2d5bea6666a152d8b1ae
gosrahul21 <gosrahul21@gmail.com>
2021-07-30 10:43:19 +0530

    ui - see more

--- src/ReviewCard.js
+++ src/ReviewCard.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './review.css'
 import StarSharpIcon from '@material-ui/icons/StarSharp';
 
 function ReviewCard({user,commentHead,commentBody,rating}) {
+    const [see,setSee] = useState(false)
     return (
-        <div className="reviewCard">
+        <div className={`reviewCard`}>
             <div className="userDetail">
                 <h2>{commentHead}</h2> 
-                <span>{user}</span>
+                <span className="user">- {user}</span>
             </div>
             <span className="rating"><StarSharpIcon style={{color:"#FFD700"}}/> {rating}/ 10</span>
             
-            <p>{commentBody}</p>
+            <p>{see?commentBody:commentBody.substring(0,400)}</p>
+            <span onClick={()=>setSee(!see)} className="link-more">{see?"read less":"read more"}</span>
         </div>
     )
 }
